399eba753fde39efab8594d6058fc3ca6bbec5b2
pythops <contact@pythops.com>
2021-05-09 22:56:59 +0200

    Add test when visualize option is false

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -1,7 +1,7 @@
 import filecmp
 from unittest.mock import patch
 
-import responses
+import responses  # type: ignore
 from click.testing import CliRunner
 
 from config import config
@@ -9,7 +9,7 @@ from ova_client import cli
 
 
 @responses.activate
-def test_detection_cli_should_produce_the_expected_image(tmpdir, monkeypatch):
+def test_detection_cli_with_visualize_option(tmpdir, monkeypatch):
     detection_response = {
         'description': 'Detected objects',
         'predictions': [
@@ -22,7 +22,25 @@ def test_detection_cli_should_produce_the_expected_image(tmpdir, monkeypatch):
 
     with patch("random.randint", return_value=0):
         runner = CliRunner()
-        result = runner.invoke(cli, ["detection", "testdata/detection/input_image.jpeg", "--save"])
+        result = runner.invoke(
+            cli, ["detection", "testdata/detection/input_image.jpeg", "--visualize", "--save"]
+        )
 
     assert result.exit_code == 0
     assert filecmp.cmp("testdata/detection/expected_image.jpeg", f"{str(tmpdir)}/input_image.jpeg")
+
+
+@responses.activate
+def test_detection_cli_without_visualize_option():
+    detection_response = {
+        'description': 'Detected objects',
+        'predictions': [
+            {'bbox': {'x1': 442, 'x2': 982, 'y1': 199, 'y2': 1270}, 'label': 'cat', 'score': '0.93'}
+        ],
+    }
+    responses.add(responses.POST, config.DETECTION_URL, json=detection_response)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["detection", "testdata/detection/input_image.jpeg"])
+
+    assert result.exit_code == 0
