057e6fad2d88d48f29ef10c6dec0bbf1cf864f4f
pythops <contact@pythops.com>
2021-05-30 12:26:24 +0200

    Draw masks instead of boxes

--- visualize.py
+++ visualize.py
@@ -3,27 +3,41 @@ import random
 from PIL import Image, ImageDraw, ImageFont
 
 
-def draw_bounding_boxe_on_image(image, bounding_boxe, score, label, color, text_offset=10):
+def get_prediction_mask(image, bounding_boxe, score, label, color, text_offset=10, opacity=96):
     x1, y1, x2, y2 = bounding_boxe
 
-    draw = ImageDraw.Draw(image)
-
-    draw.rectangle(((x1, y1), (x2, y2)), outline=color, width=3)
+    mask = Image.new("RGBA", image.size, color + (0,))
+    draw = ImageDraw.Draw(mask)
 
     font = ImageFont.truetype("fonts/Montserrat-Regular.ttf", 20)
-
     text_x, text_y = font.getsize(label)
-    draw.rectangle(((x1, y1), (x1 + text_x, y1 - text_y)), fill=color)
+    draw.rectangle(((x1, y1), (x1 + text_x, y1 - text_y)), fill=color + (opacity,))
     draw.text((x1, y1 - text_y), text=label, fill="white", font=font)
+
+    draw.rectangle(((x1, y1), (x2, y2)), fill=color + (opacity,))
+
+    return mask
+
+
+def apply_masks(image, masks):
+    for mask in masks:
+        image = Image.alpha_composite(image, mask)
+    image = image.convert("RGB")
+    return image
 
 
 def visualize_image_predictions(image, predictions):
     img = Image.open(image)
+    img = img.convert("RGBA")
+
     labels = set([prediction["label"] for prediction in predictions])
     colors = {
         label: (random.randint(0, 200), random.randint(0, 200), random.randint(0, 200))
         for label in labels
     }
+
+    masks = []
+
     for prediction in predictions:
         bounding_boxe = (
             prediction["bbox"]["x1"],
@@ -34,7 +48,12 @@ def visualize_image_predictions(image, predictions):
         score = prediction["score"]
         label = prediction["label"]
         color = colors[label]
-        draw_bounding_boxe_on_image(
+
+        mask = get_prediction_mask(
             image=img, bounding_boxe=bounding_boxe, score=score, label=label, color=color
         )
+        masks.append(mask)
+
+    img = apply_masks(img, masks)
+
     return img
